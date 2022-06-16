49848d504e770522772062c74f6a691865a433f9
Farhan Ishraq <omifarhan@gmail.com>
2022-06-16 15:30:54 +0600

    API call done

--- web/project_root/views.py
+++ web/project_root/views.py
@@ -1,7 +1,17 @@
+from django.contrib.sites import requests
 from django.shortcuts import render
+import requests
 
 
 def index(request):
-    print(request.user.username)
-    # print(request.user.email)
-    return render(request, 'index.html')
+    if request.user.is_authenticated:
+        URL = "http://127.0.0.1:8000/API/Get_Users_Courses/?username=" + request.user.username
+        headers = {'content-Type' : 'application/json'}
+        r = requests.get(url=URL, headers=headers)
+        data = r.json()
+        print(data)
+        params = {'Query': data, 'msg': 'here are your course directories!'}
+        return render(request, 'index.html', params)
+    else:
+        params = {'msg': 'Login to view your courses!'}
+        return render(request, 'index.html', params)
