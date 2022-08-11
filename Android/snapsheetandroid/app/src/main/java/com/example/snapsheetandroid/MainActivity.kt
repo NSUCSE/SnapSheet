a1a1cc92e3a9fa581df356fa5e0346628d19447d
Sarwar Khalid <56037779+Nekrodeath@users.noreply.github.com>
2022-08-12 04:32:09 +0600

    Image to text for android

---
+++ Android/snapsheetandroid/app/src/main/java/com/example/snapsheetandroid/MainActivity.kt
@@ -0,0 +1,132 @@
+package com.example.snapsheetandroid
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.Image
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.os.Environment.getExternalStoragePublicDirectory
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.jar.Manifest
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.result.Result
+
+
+class MainActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 1
+    lateinit var currentPhotoPath: String
+    lateinit var imageView: ImageView
+
+    var photoFile: File? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val click = findViewById<Button>(R.id.click)
+        click.setOnClickListener(){
+            dispatchTakePictureIntent()
+        }
+
+        imageView = findViewById<ImageView>(R.id.imageView)
+
+    }
+
+
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "SnapSheet_" + timeStamp + "_"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+            imageFileName, /* prefix */
+            ".jpg", /* suffix */
+            storageDir      /* directory */
+        )
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.absolutePath
+        return image
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                 photoFile = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.snapsheetandroid.fileprovider",
+                        it)
+
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val myBitmap = BitmapFactory.decodeFile(photoFile!!.absolutePath)
+
+
+            imageView.setImageBitmap(myBitmap)
+
+
+            var bytearray = File(photoFile!!.absolutePath).readBytes()
+
+            Fuel.post("https://api.apilayer.com/image_to_text/upload")
+                .header("apikey","jgFVyVpDtd6flOYJ4jxU7OfCRU9ZyQBJ")
+                .body(File(photoFile!!.absolutePath).readBytes())
+                .response { request,response,result ->
+                   when(result)
+                   {
+                       is Result.Success ->{
+                            println(response)
+                       }
+                       is Result.Failure ->{
+                           Toast.makeText(this,"request failed",Toast.LENGTH_LONG).show()
+                       }
+
+                   }
+                }
+
+        } else {
+
+        }
+    }
+
+
+}
