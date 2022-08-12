9ea2591679c877d461454e278d950b93265d9356
Sarwar Khalid <56037779+Nekrodeath@users.noreply.github.com>
2022-08-12 21:03:30 +0600

    Web deleted

--- web/Utils/utils.py
+++
@@ -1,5 +0,0 @@
-
-
-def get_sheet_id(url):
-    res = url.split("/")
-    return res[5]
--- web/sheets/int_to_col.py
+++
@@ -1,6 +0,0 @@
-import string
-# 1 based
-def int_to_col(n: int,b=string.ascii_uppercase) -> str:
-  n -= 1
-  d, m = divmod(n,len(b))
-  return int_to_col(d-1,b)+b[m] if d else b[m]
